1feff38bc2ecc470f7001138f657f6eb89f8fa16
TimzzOfc <107997555+Gapapapa@users.noreply.github.com>
2024-12-30 02:21:25 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,34 @@
 "use strict";
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 require('events').EventEmitter.defaultMaxListeners = 500
-const { Baileys, MongoDB, PostgreSQL, Function: Func, Config: env } = new(require('@neoxr/wb'))
-const spinnies = new(require('spinnies'))(),
+const { Baileys, MongoDB, PostgreSQL, Function: Func, Config: env } = new (require('@neoxr/wb'))
+const spinnies = new (require('spinnies'))(),
    fs = require('fs'),
    path = require('path'),
    colors = require('@colors/colors'),
    { platform } = require('os')
-const cache = new(require('node-cache'))({
+const cache = new (require('node-cache'))({
    stdTTL: env.cooldown
 })
 if (process.env.DATABASE_URL && /mongo/.test(process.env.DATABASE_URL)) MongoDB.db = env.database
-const machine = (process.env.DATABASE_URL && /mongo/.test(process.env.DATABASE_URL)) ? MongoDB : (process.env.DATABASE_URL && /postgres/.test(process.env.DATABASE_URL)) ? PostgreSQL : new(require('./lib/system/localdb'))(env.database)
+const machine = (process.env.DATABASE_URL && /mongo/.test(process.env.DATABASE_URL)) ? MongoDB : (process.env.DATABASE_URL && /postgres/.test(process.env.DATABASE_URL)) ? PostgreSQL : new (require('./lib/system/localdb'))(env.database)
 const client = new Baileys({
    type: '--neoxr-v1',
    plugsdir: 'plugins',
    sf: 'session',
    online: true,
+   bypass_disappearing: true,
    // To see the latest version : https://web.whatsapp.com/check-update?version=1&platform=web
-   version: [2, 2413, 51]
+   version: [2, 3000, 1017531287]
+}, {
+   browser: ['Ubuntu', 'Firefox', '20.0.00']
 })
 
 /* starting to connect */
-client.on('connect', async res => {
+client.once('connect', async res => {
    /* load database */
-   global.db = {users:[], chats:[], groups:[], statistic:{}, sticker:{}, setting:{}, ...(await machine.fetch() ||{})}
-   
+   global.db = { users: [], chats: [], groups: [], statistic: {}, sticker: {}, setting: {}, ...(await machine.fetch() || {}) }
+
    /* save database */
    await machine.save(global.db)
 
@@ -34,13 +37,13 @@ client.on('connect', async res => {
 })
 
 /* print error */
-client.on('error', async error => {
+client.once('error', async error => {
    console.log(colors.red(error.message))
    if (error && typeof error === 'object' && error.message) Func.logFile(error.message)
 })
 
 /* bot is connected */
-client.on('ready', async () => {
+client.once('ready', async () => {
    /* auto restart if ram usage is over */
    const ramCheck = setInterval(() => {
       var ramUsage = process.memoryUsage().rss
@@ -55,7 +58,7 @@ client.on('ready', async () => {
 
    /* additional config */
    require('./lib/system/config')
-   
+
    /* clear temp folder every 10 minutes */
    setInterval(async () => {
       try {
@@ -63,7 +66,7 @@ client.on('ready', async () => {
          if (tmpFiles.length > 0) {
             tmpFiles.filter(v => !v.endsWith('.file')).map(v => fs.unlinkSync('./temp/' + v))
          }
-      } catch {}
+      } catch { }
    }, 60 * 1000 * 10)
 
    /* save database send http-request every 30 seconds */
@@ -82,97 +85,15 @@ client.on('ready', async () => {
 })
 
 /* print all message object */
-client.on('message', ctx => {
+client.register('message', ctx => {
    require('./handler')(client.sock, ctx)
    require('./lib/system/baileys')(client.sock)
    require('./lib/system/functions')
    require('./lib/system/scraper')
 })
 
-/* print deleted message object */
-client.on('message.delete', ctx => {
-   const sock = client.sock  
-   if (!ctx || ctx.origin.fromMe || ctx.origin.isBot || !ctx.origin.sender) return
-   if (cache.has(ctx.origin.sender) && cache.get(ctx.origin.sender) === 1) return
-   cache.set(ctx.origin.sender, 1)
-   if (Object.keys(ctx.delete.message) < 1) return
-   if (ctx.origin.isGroup && global.db.groups.some(v => v.jid == ctx.origin.chat) && global.db.groups.find(v => v.jid == ctx.origin.chat).antidelete) return sock.copyNForward(ctx.origin.chat, ctx.delete)
-})
 
-/* AFK detector */
-client.on('presence.update', update => {
-   if (!update) return
-   const sock = client.sock
-   const { id, presences } = update
-   if (id.endsWith('g.us')) {
-      for (let jid in presences) {
-         if (!presences[jid] || jid == sock.decodeJid(sock.user.id)) continue
-         if ((presences[jid].lastKnownPresence === 'composing' || presences[jid].lastKnownPresence === 'recording') && global.db && global.db.users && global.db.users.find(v => v.jid == jid) && global.db.users.find(v => v.jid == jid).afk > -1) {
-            sock.reply(id, `System detects activity from @${jid.replace(/@.+/, '')} after being offline for : ${Func.texted('bold', Func.toTime(new Date - global.db.users.find(v => v.jid == jid).afk))}\n\n➠ ${Func.texted('bold', 'Reason')} : ${global.db.users.find(v => v.jid == jid).afkReason ? global.db.users.find(v => v.jid == jid).afkReason : '-'}`, global.db.users.find(v => v.jid == jid).afkObj)
-            global.db.users.find(v => v.jid == jid).afk = -1
-            global.db.users.find(v => v.jid == jid).afkReason = ''
-            global.db.users.find(v => v.jid == jid).afkObj = {}
-         }
-      }
-   } else {}
-})
 
-client.on('group.add', async ctx => {
-   const sock = client.sock
-   const text = `Thanks +tag for joining into +grup group.`
-   const groupSet = global.db.groups.find(v => v.jid == ctx.jid)
-   if (!global.db || !global.db.groups) return
-   try {
-      var pic = await sock.profilePictureUrl(ctx.member, 'image')
-      if (!pic) {
-         var pic = 'https://qu.ax/uPqo.jpg'
-      }
-   } catch {
-      var pic = 'https://qu.ax/uPqo.jpg'
-   }
-
-   /* localonly to remove new member when the number not from indonesia */
-   if (groupSet && groupSet.localonly) {
-      if (global.db.users.some(v => v.jid == ctx.member) && !global.db.users.find(v => v.jid == ctx.member).whitelist && !ctx.member.startsWith('62') || !ctx.member.startsWith('62')) {
-         sock.reply(ctx.jid, Func.texted('bold', `Sorry @${ctx.member.split`@`[0]}, this group is only for indonesian people and you will removed automatically.`))
-         sock.updateBlockStatus(member, 'block')
-         return await Func.delay(2000).then(() => sock.groupParticipantsUpdate(ctx.jid, [ctx.member], 'remove'))
-      }
-   }
-
-   const txt = (groupSet && groupSet.text_welcome ? groupSet.text_welcome : text).replace('+tag', `@${ctx.member.split`@`[0]}`).replace('+grup', `${ctx.subject}`)
-   if (groupSet && groupSet.welcome) sock.sendMessageModify(ctx.jid, txt, null, {
-      largeThumb: true,
-      thumbnail: pic,
-      url: global.db.setting.link
-   })
-})
-
-client.on('group.remove', async ctx => {
-   const sock = client.sock
-   const text = `Good bye +tag :)`
-   if (!global.db || !global.db.groups) return
-   const groupSet = global.db.groups.find(v => v.jid == ctx.jid)
-   try {
-      var pic = await sock.profilePictureUrl(ctx.member, 'image')
-      if (!pic) {
-         var pic = 'https://qu.ax/uPqo.jpg'
-      }
-   } catch {
-      var pic = 'https://qu.ax/uPqo.jpg'
-   }
-   const txt = (groupSet && groupSet.text_left ? groupSet.text_left : text).replace('+tag', `@${ctx.member.split`@`[0]}`).replace('+grup', `${ctx.subject}`)
-   if (groupSet && groupSet.left) sock.sendMessageModify(ctx.jid, txt, null, {
-      largeThumb: true,
-      thumbnail: pic,
-      url: global.db.setting.link
-   })
-})
-
-client.on('caller', ctx => {
-	if (typeof ctx === 'boolean') return
-	client.sock.updateBlockStatus(ctx.jid, 'block')
-})
 
 // client.on('group.promote', ctx => console.log(ctx))
 // client.on('group.demote', ctx => console.log(ctx))
