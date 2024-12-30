2136461f5e36abd99f7aef2e094c78f638b736b5
TimzzOfc <107997555+Gapapapa@users.noreply.github.com>
2024-12-30 08:31:59 +0700

    Update fromai.js

--- plugins/misc/fromai.js
+++ plugins/misc/fromai.js
@@ -1,10 +1,6 @@
 exports.run = {
    async: async (m, { client, body }) => {
-      try {
-         global.db.settings = global.db.settings || {}
-         const settings = global.db.settings
-         settings.online = settings.online || false // Default offline jika tidak diatur
-         if (!settings.online) return // Jika fitur online nonaktif, bot tidak merespon
+      try {         
          const d = new Date(new Date() + 12 * 60 * 60 * 1000) // 12 jam
          let week = d.toLocaleDateString('id', { weekday: 'long' }) 
          let date = d.toLocaleDateString('id', {
@@ -22,7 +18,7 @@ exports.run = {
          let cur = user.time + 12 * 60 * 60 * 1000 // 12 jam         
          if (m.text && (!user.time || user.time === 0 || cur - new Date() <= 0)) {
             user.time = new Date().getTime()
-            client.sendFromAI(m.chat, message)
+            client.sendFromAI(m.chat, message, null)
          }
       } catch (e) {
          return client.reply(m.chat, Func.jsonFormat(e), m)
